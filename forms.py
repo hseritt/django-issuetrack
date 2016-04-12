46e6879c8e9aef12bf96713cb3d080286e49125c
hseritt <harlin.seritt@gmail.com>
2016-04-11 20:38:58 -0600

    Fixed Issue #14

--- forms.py
+++ forms.py
@@ -12,6 +12,8 @@ class AddIssueForm(ModelForm):
 		super(AddIssueForm, self).__init__(*args, **kwargs)
 		
 		if not user.is_staff or not user.is_superuser:
+			''' Don't show assignee field unless user is staff user or superuser.
+			'''
 			del(self.fields['assignee'])
 	
 	class Meta:
@@ -26,6 +28,7 @@ class AddIssueForm(ModelForm):
 	def clean(self):
 		
 		cleaned_data = super(AddIssueForm, self).clean()
+		
 		kind = cleaned_data.get('kind')
 		steps = cleaned_data.get('steps')
 		observed = cleaned_data.get('observed')
@@ -55,7 +58,9 @@ class ChangeIssueForm(ModelForm):
 class AddProjectForm(ModelForm):
 
 	def __init__(self, *args, **kwargs):
+		
 		super(AddProjectForm, self).__init__(*args, **kwargs)
+		
 		self.fields['description'].widget.attrs.update(
 			{
 				'class': 'mceNoEditor',
@@ -74,7 +79,9 @@ class AddProjectForm(ModelForm):
 class ChangeProjectForm(ModelForm):
 
 	def __init__(self, *args, **kwargs):
+		
 		super(ChangeProjectForm, self).__init__(*args, **kwargs)
+		
 		self.fields['description'].widget.attrs.update(
 			{
 				'class': 'mceNoEditor',
@@ -93,7 +100,9 @@ class ChangeProjectForm(ModelForm):
 class AddComponentForm(ModelForm):
 
 	def __init__(self, *args, **kwargs):
+		
 		super(AddComponentForm, self).__init__(*args, **kwargs)
+		
 		self.fields['description'].widget.attrs.update(
 			{
 				'class': 'mceNoEditor',
@@ -112,7 +121,9 @@ class AddComponentForm(ModelForm):
 class ChangeComponentForm(ModelForm):
 
 	def __init__(self, *args, **kwargs):
+		
 		super(ChangeComponentForm, self).__init__(*args, **kwargs)
+		
 		self.fields['description'].widget.attrs.update(
 			{
 				'class': 'mceNoEditor',
@@ -136,6 +147,7 @@ class AddCommentForm(ModelForm):
 	)
 
 	def __init__(self, *args, **kwargs):
+		
 		try:
 			self.issue_status = kwargs.pop('issue_status')
 		except KeyError:
@@ -160,6 +172,7 @@ class AddCommentForm(ModelForm):
 class ChangeCommentForm(AddCommentForm):
 
 	def __init__(self, *args, **kwargs):
+		
 		try:
 			self.issue_status = kwargs.pop('issue_status')
 		except KeyError:
