967459c70aca394ecd3bba515a3fabc1f919e325
hseritt <harlin.seritt@gmail.com>
2021-09-28 17:23:37 -0600

    Made compatible with new Django version

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ from issuetrack.settings import ISSUE_URGENCIES, COMMENT_AUDIENCES
 
 
 class Project(models.Model):
-    ''' Project can be something like a software application or ongoing
+    '''Project can be something like a software application or ongoing
     event or project.
     '''
 
@@ -18,7 +18,7 @@ class Project(models.Model):
     key = models.CharField('Key', max_length=10, unique=True)
     ''' A unique 10 letter (or less) key for the project.
     '''
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     ''' A technical user who is the manager/owner or main stakeholder for
     the project.
     '''
@@ -31,20 +31,19 @@ class Project(models.Model):
     '''
 
     def __str__(self):
-        ''' String repr of the Project is its name.
-        '''
+        '''String repr of the Project is its name.'''
         return self.name
 
 
 class Component(models.Model):
-    ''' A particular area of a project. For example a component for a
+    '''A particular area of a project. For example a component for a
     software project could be 'models' or 'database' or 'UI'.
     '''
 
     name = models.CharField('Name', max_length=30)
     ''' The component's name.
     '''
-    project = models.ForeignKey(Project)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     ''' Associated project.
     '''
     description = models.TextField('Description', null=True, blank=True)
@@ -52,21 +51,15 @@ class Component(models.Model):
     '''
 
     class Meta:
-        ''' Meta properties of the Component class go here.
-        '''
-        unique_together = (
-            ('name', 'project')
-        )
+        '''Meta properties of the Component class go here.'''
+
+        unique_together = ('name', 'project')
         ''' The name and project must be unique together.
         '''
 
     def __str__(self):
-        ''' String repr of the Component.
-        '''
-        return '{}/{}'.format(
-            self.project.name,
-            self.name
-        )
+        '''String repr of the Component.'''
+        return '{}/{}'.format(self.project.name, self.name)
 
 
 class Issue(models.Model):
@@ -79,57 +72,55 @@ class Issue(models.Model):
     '''
 
     steps = models.TextField(
-        'Steps to Replicate Issue', null=True, blank=True,
-        help_text='These are reliable steps that when followed will' +
-        'replicate your issue. Not required if the description covers' +
-        'everything necessary for describing the issue.',
+        'Steps to Replicate Issue',
+        null=True,
+        blank=True,
+        help_text='These are reliable steps that when followed will'
+        + 'replicate your issue. Not required if the description covers'
+        + 'everything necessary for describing the issue.',
     )
 
     observed = models.TextField(
-        'Observed Behavior', null=True, blank=True,
-        help_text='This is the behavior you are observing when following' +
-        ' the steps above.')
+        'Observed Behavior',
+        null=True,
+        blank=True,
+        help_text='This is the behavior you are observing when following'
+        + ' the steps above.',
+    )
 
     expected = models.TextField(
-        'Expected Behavior', null=True, blank=True,
-        help_text='This is the behavior you are reasonably expecting to ' +
-        'occur when following the steps above.')
-
-    kind = models.CharField(
-        'Kind',
-        max_length=30,
-        choices=ISSUE_KINDS,
+        'Expected Behavior',
+        null=True,
+        blank=True,
+        help_text='This is the behavior you are reasonably expecting to '
+        + 'occur when following the steps above.',
     )
+
+    itype = models.CharField('Type', max_length=30, choices=ISSUE_KINDS)
     ''' The kind or type of issue.
     '''
-    priority = models.CharField(
-        'Priority',
-        max_length=30,
-        choices=ISSUE_PRIORITIES,
-    )
+    priority = models.CharField('Priority', max_length=30, choices=ISSUE_PRIORITIES)
     ''' The priority for the issue.
     '''
-    urgency = models.CharField(
-        'Urgency',
-        max_length=30,
-        choices=ISSUE_URGENCIES,
-    )
+    urgency = models.CharField('Urgency', max_length=30, choices=ISSUE_URGENCIES)
     ''' The urgency for the issue. Sets an expectation of when an issue
     should be resolved.
     '''
     status = models.CharField(
-        'Status',
-        max_length=30,
-        choices=ISSUE_STATUSES,
-        default='New',
+        'Status', max_length=30, choices=ISSUE_STATUSES, default='New'
     )
     ''' The status or disposition of the issue.
     '''
-    creater = models.ForeignKey(User)
+    creater = models.ForeignKey(User, on_delete=models.CASCADE)
     ''' The user who created the issue.
     '''
     assignee = models.ForeignKey(
-        User, null=True, blank=True, related_name='issue_assignee')
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='issue_assignee',
+    )
     ''' The user who is assigned as the agent responsible for disposition of
      the issue.
     '''
@@ -139,26 +130,27 @@ class Issue(models.Model):
     modified = models.DateTimeField(auto_now=True)
     ''' The date and time when the issue was last updated.
     '''
-    component = models.ForeignKey(Component, null=True, blank=True)
+    component = models.ForeignKey(
+        Component, on_delete=models.CASCADE, null=True, blank=True
+    )
     ''' Associates with the Component.
     '''
 
     def __str__(self):
-        ''' String repr of the issue using the issue's title.
-        '''
+        '''String repr of the issue using the issue's title.'''
         return self.title
 
 
 class Comment(models.Model):
-    ''' Associated comment for an issue.
-    '''
-    issue = models.ForeignKey(Issue)
+    '''Associated comment for an issue.'''
+
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
     ''' Associated issue the comment is for.
     '''
     text = models.TextField('Comment')
     ''' The comment's text.
     '''
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     ''' User who created the comment.
     '''
     created = models.DateTimeField(auto_now_add=True)
@@ -168,10 +160,7 @@ class Comment(models.Model):
     ''' The date and time when the comment was last modified.
     '''
     audience = models.CharField(
-        'Audience',
-        max_length=30,
-        choices=COMMENT_AUDIENCES,
-        default='Private',
+        'Audience', max_length=30, choices=COMMENT_AUDIENCES, default='Private'
     )
     ''' Who the comment is intended for.
     '''
@@ -179,8 +168,8 @@ class Comment(models.Model):
     issue_status = models.CharField('Current Issue Status', max_length=30)
 
     def __str__(self):
-        ''' String repr of the comment. Includes the issue title, comment
-         author and the date and time when the comment was created.
+        '''String repr of the comment. Includes the issue title, comment
+        author and the date and time when the comment was created.
         '''
         return 'Comment for "{}" by {} at {}'.format(
             self.issue.title, self.author, self.created
